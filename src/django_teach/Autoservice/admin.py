e4eeee297f5773aaf1662ee80b1229fb2bdb16aa
1novel8 <18.novel.18@gmail.com>
2023-06-10 19:22:12 +0300

    add admin panel

--- src/django_teach/Autoservice/admin.py
+++ src/django_teach/Autoservice/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from Autoservice import models
 
-# Register your models here.
+
+@admin.register(models.CarModel)
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in models.CarModel._meta.fields]
+    list_filter = [field.name for field in models.CarModel._meta.fields]
+    search_fields = [field.name for field in models.CarModel._meta.fields]
+    fields = [field.name for field in models.CarModel._meta.fields]
+    readonly_fields = ('id', 'is_active', 'created_at', 'updated_at')
+    ordering = [field.name for field in models.CarModel._meta.fields]
+    pass
+
+
+class PreferredParametersModelAdmin(admin.StackedInline):
+    model = models.PreferredCarParametersModel
+
+
+@admin.register(models.AutoserviceModel)
+class AutoserviceModelAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in models.AutoserviceModel._meta.fields]
+    list_filter = [field.name for field in models.AutoserviceModel._meta.fields]
+    search_fields = [field.name for field in models.AutoserviceModel._meta.fields]
+    fields = [field.name for field in models.AutoserviceModel._meta.fields]
+    readonly_fields = ('id', 'is_active', 'created_at', 'updated_at')
+    ordering = [field.name for field in models.AutoserviceModel._meta.fields]
+
+    inlines = [PreferredParametersModelAdmin]
+
+
+@admin.register(models.PreferredCarParametersModel)
+class AutoserviceModelAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in models.PreferredCarParametersModel._meta.fields]
+    list_filter = [field.name for field in models.PreferredCarParametersModel._meta.fields]
+    search_fields = [field.name for field in models.PreferredCarParametersModel._meta.fields]
+    fields = [field.name for field in models.PreferredCarParametersModel._meta.fields]
+    readonly_fields = ('id',)
+    ordering = [field.name for field in models.PreferredCarParametersModel._meta.fields]
+
+
+
+
--- src/django_teach/Autoservice/models.py
+++ src/django_teach/Autoservice/models.py
@@ -1,5 +1,45 @@
 from django.db import models
+from django_countries.fields import CountryField
+
+from basemodels import BaseModel
+from enums import Brand, FuelType
 
 
-class AutoserviceModel(models.Model):
-    pass
+class CarModel(BaseModel):
+    name = models.CharField(max_length=20)
+    brand = models.CharField(choices=Brand.choices())
+    fuel_type = models.CharField(choices=FuelType.choices())
+
+    class Meta:
+        db_table = 'car'
+
+    def __str__(self):
+        return f'id:{self.id}, name:{self.name}, brand:{self.brand}, fuel:{self.fuel_type}'
+
+
+class AutoserviceModel(BaseModel):
+    name = models.CharField(max_length=20, verbose_name='name')
+    balance = models.IntegerField(default=0, verbose_name='balance')
+    country = CountryField()
+
+    class Meta:
+        db_table = 'autoservice'
+
+    def __str__(self):
+        return f'id:{self.id}, name:{self.name}, balance:{self.balance}, country:{self.country}'
+
+
+class PreferredCarParametersModel(models.Model):
+    autoservice = models.OneToOneField(
+        AutoserviceModel,
+        on_delete=models.CASCADE,
+        related_name='preferred_car_parameters'
+    )
+    brand = models.CharField(choices=Brand.choices())
+    fuel_type = models.CharField(choices=FuelType.choices())
+
+    class Meta:
+        db_table = 'preferred_car_parameters'
+
+    def __str__(self):
+        return f'autoservice_id:{self.autoservice}, brand:{self.brand}, fuel:{self.fuel_type}'
